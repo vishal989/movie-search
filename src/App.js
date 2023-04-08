b183f521875137911543a04599b4ab980c93028e
vishal989 <vavishalarya@gmail.com>
2023-04-09 02:22:02 +0530

    filtering and more improvement in styling

--- src/App.js
+++ src/App.js
@@ -5,12 +5,22 @@ import MovieList from './components/MovieList';
 import MovieDetails from './components/MovieDetails';
 import BackToTop from './components/BackToTop';
 import './style.css';
+import Genre from './components/Genre';
+import Genres from './components/Genres';
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerms, setSearchTerms] = useState('');
   const [cache, setCache] = useState({});
   const [pageCount, setPageCount] = useState(1);
+  const [genre, setGenre] = useState([]);
+  const [clickEvent, setClickEvent] = useState(false);
+  const fetchGenre = (g) => {
+    setGenre(g);
+    setClickEvent(true);
+  };
+
+  // console.log(genre);
 
   const fetchMovies = async (searchTerms, pageCount) => {
     const URL = `http://www.omdbapi.com/?s=${searchTerms}&page=${pageCount}&apikey=60c72e71`;
@@ -33,19 +43,46 @@ const App = () => {
 
   return (
     <div className="App">
-      <BackToTop/>
+      <BackToTop />
+
       <BrowserRouter>
         <Routes>
           <Route
-            path="/"
+            path="/genre"
             element={
-              <MovieList
+              <Genres
                 movies={movies}
                 searchTerms={searchTerms}
                 setSearchTerms={setSearchTerms}
                 pageCount={pageCount}
                 setPageCount={setPageCount}
+                genre={genre}
+                clickEvent={clickEvent}
+                setClickEvent={setClickEvent}
               />
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <>
+                <Genre
+                  movies={movies}
+                  fetchGenre={fetchGenre}
+                  clickEvent={clickEvent}
+                  setClickEvent={setClickEvent}
+                />
+                <MovieList
+                  movies={movies}
+                  searchTerms={searchTerms}
+                  setSearchTerms={setSearchTerms}
+                  pageCount={pageCount}
+                  setPageCount={setPageCount}
+                  genre={genre}
+                  clickEvent={clickEvent}
+                  setClickEvent={setClickEvent}
+                />
+              </>
             }
           />
           <Route path="/:id" element={<MovieDetails />} />
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,39 +1,59 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import SearchBar from './SearchBar';
 import { useNavigate } from 'react-router-dom';
 
 const MovieList = (props) => {
   const navigate = useNavigate();
+  var query;
+  if (props.clickEvent) {
+    query = props.genre;
+    // props.setClickEvent(false);
+  } else {
+    query = props.movies;
+  }
+
   const routeChange = (id) => {
     const path = `/${id}`;
     navigate(path);
   };
 
+  console.log(query);
+
+  useEffect(() => {
+    query = props.genre;
+  }, [props.clickEvent]);
+
   return (
     <div className="movie-list">
-      <SearchBar
-        setPageCount={props.setPageCount}
-        searchTerms={props.searchTerms}
-        setSearchTerms={props.setSearchTerms}
-      />
-      <div>
-        <button
-          onClick={
-            props.pageCount === 1
-              ? () => props.setPageCount(1)
-              : () => props.setPageCount(props.pageCount - 1)
-          }
-        >
-          prev
-        </button>
-        <button onClick={() => props.setPageCount(props.pageCount + 1)}>
-          Next
-        </button>
-      </div>
-      <div className='movie-list-wrapper'>
-        {props.movies?.map((movie) => (
+      {!props.clickEvent && (
+        <SearchBar
+          setPageCount={props.setPageCount}
+          searchTerms={props.searchTerms}
+          setSearchTerms={props.setSearchTerms}
+        />
+      )}
+      {!props.clickEvent && (
+        <div>
+          <button
+            onClick={
+              props.pageCount === 1
+                ? () => props.setPageCount(1)
+                : () => props.setPageCount(props.pageCount - 1)
+            }
+          >
+            Prev
+          </button>
+          <button onClick={() => props.setPageCount(props.pageCount + 1)}>
+            Next
+          </button>
+        </div>
+      )}
+      <div className="movie-list-wrapper">
+        
+        {query?.map((movie) => (
           <div
             key={movie.imdbID}
+            id={movie.imdbID}
             className="movie-list-card"
             onClick={() => routeChange(movie.imdbID)}
           >
